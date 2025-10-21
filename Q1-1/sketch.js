b8e024122651c0b4081533c15517a1e8e8d6d3e7
popmusic3553 <popmusic3553@gmail.com>
2025-10-21 11:21:42 +0900

    Change section1-1

--- Q1-1/sketch.js
+++ Q1-1/sketch.js
@@ -7,5 +7,5 @@ function setup(){
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1] 空欄を埋めて神戸市のロゴマークを完成させよう
+  // BLANK[1] 空欄を埋めて神戸市のロゴマークを完成させろ
 }
